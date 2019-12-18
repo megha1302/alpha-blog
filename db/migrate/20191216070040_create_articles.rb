216206d9fd5b45591a97e8240d6c70aed6b8e657
megha1302 <megha.sonkusare@anchanto.com>
2019-12-18 15:59:08 +0530

    user authentication

--- db/migrate/20191216070040_create_articles.rb
+++ db/migrate/20191216070040_create_articles.rb
@@ -1,8 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
+    
     create_table :articles do |t|
       t.string :title
       t.text :description
+      
     end
+    add_column :articles, :user_id, :integer
   end
 end
